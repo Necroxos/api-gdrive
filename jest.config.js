d2098f695e981ccb807fda4427fd94fcfbd8fd3d
Rodrigo Dorat <Rodrigo.Dorat@walmart.com>
2022-07-01 10:33:41 -0400

    🐛 Fix bugs

--- jest.config.js
+++ jest.config.js
@@ -6,16 +6,12 @@ module.exports = {
     roots: ['test'],
     bail: 1,
     verbose: true,
-    collectCoverageFrom: ['**/*.{ts,tsx}', '!**/node_modules/**'],
     coverageDirectory: './coverage',
     collectCoverageFrom: [
-        "./src/**/*.ts",
-        "!src/config/*.ts",
+        "./src/**/*.{ts,tsx}",
+        '!**/node_modules/**',
         "!src/server/enums/*.ts",
         "!src/server/constants/*.ts",
         "!src/server/interfaces/*.ts"
-    ],
-    transformIgnorePatterns: [
-        "src/config/*.ts"
     ]
 };
